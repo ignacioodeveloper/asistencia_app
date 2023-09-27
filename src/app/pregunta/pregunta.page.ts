0f077e3434fcef25445ee0754bd4ce334d43f747
Luis <luisignaciocontacto@gmail.com>
2023-09-27 17:04:16 -0300

    v1

---
+++ src/app/pregunta/pregunta.page.ts
@@ -0,0 +1,45 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { Usuario } from 'src/app/model/usuario';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pregunta',
+  templateUrl: './pregunta.page.html',
+  styleUrls: ['./pregunta.page.scss'],
+})
+export class PreguntaPage implements OnInit {
+
+  public usuario!: Usuario;
+  public respuesta: string = '';
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+
+    )
+  {
+
+
+    this.activatedRoute.queryParams.subscribe(params => {
+      const currentNavigation = this.router.getCurrentNavigation();
+      if (currentNavigation && currentNavigation.extras.state) {
+        this.usuario = currentNavigation.extras.state["usuario"];
+      } else {
+        this.router.navigate(['/login']);
+      }
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  public validarRespuestaSecreta(): void {
+    if (this.usuario.respuestaSecreta === this.respuesta) {
+      alert('CORRECTO!!! TU CLAVE ES ' + this.usuario.password);
+    }
+    else {
+      alert('INCORRECTO!!!');
+    }
+  }
+
+}
